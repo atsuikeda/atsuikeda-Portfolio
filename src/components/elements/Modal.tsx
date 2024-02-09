32ab0f595c7cc2f9b5dafd300d0f34099d5154e3
atsuikeda <ike.ke.no.ke@gmail.com>
2024-02-09 18:59:43 +0900

    modal close button実装

--- src/components/elements/Modal.tsx
+++ src/components/elements/Modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef } from "react";
 
@@ -28,7 +30,10 @@ export default function Modal({ children }: { children: React.ReactNode }) {
       className="z-30 w-full h-full fixed top-0 left-0 bg-black bg-opacity-50"
       onClick={handleModalClose}
     >
-      <dialog ref={ref} className="w-11/12 max-w-3xl rounded-lg">
+      <dialog ref={ref} className="relative w-11/12 max-w-3xl rounded-lg">
+        <button onClick={handleModalClose} className="absolute top-4 right-4 z-10">
+        <FontAwesomeIcon icon={faXmark} className="h-7 hover:opacity-80" color="#1F2937" />
+        </button>
         <div onClick={(e) => e.stopPropagation()}>{children}</div>
       </dialog>
     </div>
